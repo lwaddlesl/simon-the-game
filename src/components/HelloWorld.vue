8e38b56ac9c2d620e95ba38ae0083966e32d9ff0
Nikita <nekit.zernov@yandex.ru>
2022-02-23 00:56:18 +0300

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,210 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="container">
+      <ul>
+        <li class="red" @click="panelClicked($event, 'red.mp3')" ref="red"></li>
+        <li
+          class="blue"
+          @click="panelClicked($event, 'blue.mp3')"
+          ref="blue"
+        ></li>
+        <li
+          class="yellow"
+          @click="panelClicked($event, 'yellow.mp3')"
+          ref="yellow"
+        ></li>
+        <li
+          class="green"
+          @click="panelClicked($event, 'green.mp3')"
+          ref="green"
+        ></li>
+      </ul>
+    </div>
+    <div class="buttons">
+      round:{{ sequence.length }}
+      <div>
+        <button :disabled="statrted" @click="start(1500)">легкий</button>
+      </div>
+      <div>
+        <button :disabled="statrted" @click="start(1000)">нормальный</button>
+      </div>
+      <div>
+        <button :disabled="statrted" @click="start(400)">сложный</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      sequence: [],
+      sequenceTuGuess: [],
+      canClick: false,
+      time: 0,
+      statrted: false,
+    };
+  },
+  watch: {
+    canClick(value) {
+      value ? this.clickHandler("add") : this.clickHandler("remove");
+    },
+  },
+  methods: {
+    change() {
+      console.log("canClick");
+    },
+    getRandomPanel() {
+      const red = this.$refs.red;
+      const blue = this.$refs.blue;
+      const yellow = this.$refs.yellow;
+      const green = this.$refs.green;
+      const panels = [red, blue, yellow, green];
+      return panels[parseInt(Math.random() * panels.length)];
+    },
+    async startFlashing() {
+      this.canClick = false;
+      for (const panel of this.sequence) {
+        await this.flash(panel);
+      }
+      this.canClick = true;
+      this.sequenceTuGuess = [...this.sequence];
+    },
+    flash(panel) {
+      return new Promise((resolve) => {
+        panel.classList.toggle("active");
+        var audio = new Audio();
+        audio.src = require(`@/assets/${panel.classList[0]}.mp3`);
+        audio.autoplay = true;
+        setTimeout(() => {
+          panel.classList.toggle("active");
+          setTimeout(() => {
+            resolve();
+          }, 250);
+        }, this.time);
+      });
+    },
+    start(time) {
+      this.sequence = [this.getRandomPanel()];
+      this.time = time;
+      this.startFlashing();
+      this.statrted = true;
+    },
+    clickHandler(a) {
+      const red = this.$refs.red;
+      const blue = this.$refs.blue;
+      const yellow = this.$refs.yellow;
+      const green = this.$refs.green;
+      const panels = [red, blue, yellow, green];
+      if (a == "add") {
+        for (let i = 0; i < panels.length; i++) {
+          panels[i].classList.add("on-click");
+        }
+      } else {
+        for (let i = 0; i < panels.length; i++) {
+          panels[i].classList.remove("on-click");
+        }
+      }
+    },
+    panelClicked(panelClicked, audioPath) {
+      if (!this.canClick) return;
+      var audio = new Audio();
+      audio.src = require(`@/assets/${audioPath}`);
+      audio.autoplay = true;
+      const expectedPanel = this.sequenceTuGuess.shift();
+      if (expectedPanel == panelClicked.path[0]) {
+        if (this.sequenceTuGuess.length == 0) {
+          this.sequence.push(this.getRandomPanel());
+          return new Promise((resolve) => {
+            this.canClick = false;
+            setTimeout(() => {
+              this.startFlashing();
+              resolve();
+            }, 1000);
+          });
+        }
+      } else {
+        alert("game over");
+        this.sequence = [];
+        this.canClick = false;
+        this.statrted = false;
+      }
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.buttons {
+  margin-left: 100px;
 }
+.container {
+  margin-left: 30vw;
+  margin-top: 10vw;
+}
+h1 {
+  margin: 1em 0 2em;
+  text-align: center;
+}
+.on-click:active {
+  opacity: 1;
+}
+
 ul {
-  list-style-type: none;
-  padding: 0;
+  list-style: none;
 }
+
+ul,
 li {
-  display: inline-block;
-  margin: 0 10px;
+  padding: 0;
+  margin: 0;
 }
-a {
-  color: #42b983;
+.active {
+  opacity: 1 !important;
+}
+.red,
+.blue,
+.yellow,
+.green {
+  height: 290px;
+  -webkit-border-radius: 150px 150px 150px 150px;
+  border-radius: 150px 150px 150px 150px;
+  position: absolute;
+}
+
+.red:hover,
+.blue:hover,
+.yellow:hover,
+.green:hover {
+  border: 2px solid black;
+}
+
+.red {
+  background: red;
+  opacity: 0.3;
+  clip: rect(0px, 300px, 150px, 150px);
+  width: 296px;
+}
+
+.blue {
+  background: blue;
+  opacity: 0.3;
+  clip: rect(0px, 150px, 150px, 0px);
+  width: 300px;
+}
+
+.yellow {
+  background: yellow;
+  opacity: 0.3;
+  clip: rect(150px, 150px, 300px, 0px);
+  width: 300px;
+}
+
+.green {
+  background: greenyellow;
+  opacity: 0.3;
+  clip: rect(150px, 300px, 300px, 150px);
+  width: 296px;
 }
 </style>
